735f16df6ac6497c64252bd262c7b565349f36ba
AnanyaKat <ananyakatta1995@gmail.com>
2025-09-10 14:47:28 +0000

    Updated

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+let name = new String("Ananyq")
+console.log(name);
+
+// name = name.toUpperCase()
+console.log(name.toUpperCase());
+console.log(`My name ${name.toLowerCase()} is misplelled`);
+
+
+
