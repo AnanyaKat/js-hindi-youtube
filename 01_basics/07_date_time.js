f7c487c66e5f64b540e9b7c33b765039f9472361
AnanyaKat <ananyakatta1995@gmail.com>
2025-09-13 17:01:30 +0000

    New files were added

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,8 @@
+// Date
+
+let myDate = new Date()
+// console.log(myDate.toDateString());
+
+newDate = new Date(2023,0,25)
+console.log(newDate.getMonth());
+
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,4 @@
+
+array1 = [1,2,3,[4,5,[6,7,[8,9]]]]
+array2 = array1.flat(2)
+console.log(array2);
